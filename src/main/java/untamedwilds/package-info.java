1e898da99f77a20c7798b6b8683d019c93fbd796
shadowUta <182259078+shadowUta@users.noreply.github.com>
2026-08-18 14:33:43 +0800

    Restore excluded sources and migrate villager trades

--- src/main/java/untamedwilds/package-info.java
+++
@@ -1,8 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
-package untamedwilds;
-
-import com.mojang.blaze3d.FieldsAreNonnullByDefault;
-import com.mojang.blaze3d.MethodsReturnNonnullByDefault;
-import javax.annotation.ParametersAreNonnullByDefault;
